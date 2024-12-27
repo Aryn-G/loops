a77c57c0257f05ea55f5bc6e8feebe4bb4f85077
Aryan Gera <aryangera@hotmail.com>
2024-12-27 17:12:45 -0500

    fixed opensearch shortname

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           rel="search"
           type="application/opensearchdescription+xml"
           href="/opensearch.xml"
-          title="ShortName"
+          title="Search Loops"
         />
       </head>
       <body
--- src/app/opensearch.xml/route.ts
+++ src/app/opensearch.xml/route.ts
@@ -6,7 +6,7 @@ export async function GET(req: NextRequest) {
   console.log(baseUrl);
 
   const xml = `<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/" xmlns:moz="http://www.mozilla.org/2006/browser/search/">
-  <ShortName>Loops</ShortName>
+  <ShortName>Search Loops</ShortName>
   <Description>Search NCSSM Loops</Description>
   <InputEncoding>UTF-8</InputEncoding>
   <Image width="16" height="16" type="image/x-icon">${baseUrl}/favicon.ico</Image>
